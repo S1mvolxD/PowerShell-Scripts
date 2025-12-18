1af8206dd29c8877b35dc9622a29ddd08530156b
Simvol <144470909+S1mvolxD@users.noreply.github.com>
2025-12-18 21:25:17 +0700

    Add files via upload

---
+++ Clear-Temp.ps1
@@ -0,0 +1,20 @@
+﻿# Clear-Temp.ps1 (v0.1) - A very basic option.
+Write-Host "The script for cleaning temporary files is running." -ForegroundColor Green
+
+# 1. Defining the target folder
+$targetFolder = $env:TEMP
+
+# 2. We show you what's in it.
+Write-Host "Содержимое папки $($targetFolder):" -ForegroundColor Yellow
+Get-ChildItem -Path $targetFolder
+
+# 3. Request confirmation from the user
+$userAnswer = Read-Host "Do you really want to delete these files? (Yes/No)"
+
+if ($userAnswer -eq 'Yes') {
+    # 4. Deleting it!
+    Remove-Item -Path "$($targetFolder)\*" -Force -Recurse -ErrorAction SilentlyContinue
+    Write-Host "The folder has been cleared!" -ForegroundColor Green
+}  else {
+    Write-Host "The operation was canceled by the user." -ForegroundColor Red
+}
